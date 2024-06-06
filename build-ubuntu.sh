d4543b47ecbb386e76c0afc17fbb77c191b61a59
Brent Pappas <pappasbrent@gmail.com>
2024-06-06 19:48:17 -0400

    Provide separate build scripts for Ubuntu and Mac

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,6 +1,6 @@
 cmake -S . -B build -G "Ninja Multi-Config" \
-    -D CMAKE_C_COMPILER=clang \
-    -D CMAKE_CXX_COMPILER=clang++ \
+    -D CMAKE_C_COMPILER=/usr/bin/clang-17 \
+    -D CMAKE_CXX_COMPILER=/usr/bin/clang-17++ \
     -D LLVM_DIR=/usr/lib/llvm-17/lib/cmake/llvm \
     -D Clang_DIR=/usr/lib/llvm-17/lib/cmake/clang \
     -D CMAKE_EXPORT_COMPILE_COMMANDS=TRUE
