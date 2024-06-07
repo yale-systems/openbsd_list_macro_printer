90c01d8c9780c4b1e76938e375ae65c5cf0d5b09
Brent Pappas <pappasbrent@gmail.com>
2024-06-06 20:05:26 -0400

    Remove extra slash in LLVM_DIR path

--- build-mac.sh
+++ build-mac.sh
@@ -1,7 +1,7 @@
 cmake -S . -B build -G "Ninja Multi-Config" \
     -D CMAKE_C_COMPILER=/opt/homebrew/opt/llvm@17/bin/clang \
     -D CMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm@17/bin/clang++ \
-    -D LLVM_DIR=/opt/homebrew/Cellar/llvm@17/17.0.6/lib/cmake//llvm \
+    -D LLVM_DIR=/opt/homebrew/Cellar/llvm@17/17.0.6/lib/cmake/llvm \
     -D Clang_DIR=/opt/homebrew/Cellar/llvm@17/17.0.6/lib/cmake/clang \
     -D CMAKE_EXPORT_COMPILE_COMMANDS=TRUE
 
