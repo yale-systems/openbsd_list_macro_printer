4aab21aa8ba94a7e1ed49b3d9961f54349e0d09b
Robert Soule <robert.soule@yale.edu>
2024-06-12 16:16:47 -0400

    more warnings

--- run-on-proc.sh
+++ run-on-proc.sh
@@ -1,6 +1,9 @@
 /usr/local/llvm17/bin/clang \
     -fplugin=./build/lib/Debug/libopenbsd_list_macro_printer.so \
     -fsyntax-only \
+    -Wno-implicit-int \
+    -Wno-implicit-function-declaration \
+    -Wno-macro-redefined \
     -I /usr/src/sys \
     -I /usr/obj/usr/src/arm64.aarch64/tmp/usr/include \
     /usr/src/sys/kern/kern_proc.c
