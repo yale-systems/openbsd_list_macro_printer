8a002e95a91e417c8ff0fbbce5052863e777e129
Robert Soule <robert.soule@yale.edu>
2024-06-12 16:11:55 -0400

    closer to running on bsd

--- run-on-proc.sh
+++ run-on-proc.sh
@@ -1,9 +1,13 @@
 /usr/local/llvm17/bin/clang \
     -fplugin=./build/lib/Debug/libopenbsd_list_macro_printer.so \
     -fsyntax-only \
-    -nostdinc \
-    -nostdlib \
+    -I /usr/src/sys \
+    -I /usr/obj/usr/src/arm64.aarch64/tmp/usr/include \
     /usr/src/sys/kern/kern_proc.c
+
+
+    #-nostdinc \
+    #-nostdlib \
 
 
 
